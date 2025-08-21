baac90ab336d90d86c7378c801c142442a37e87d
ankitpadey860 <ankitp6040@gmail.com>
2025-08-21 20:29:56 +0530

    notess day 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
 function App() {
   return (
-    
+    /*
       <h1>hello world</h1>
-
+      <h1> this will show an error </h1>
+    */
+   /*this is correct way */
+    <div>
+      <h1> we are in correct block</h1>
+      <h1>no error found </h1>
+        <Demo />
+        <Demo2 />
+        <Comp />
+         <h1>{func()}</h1>   {/* yeh line func() ka return UI me dikhayegi */}
+    </div>
+  
   )
 }
+function Demo(){
+  return(
+    <div>
+    <h1> muliple component bnake unhe execute krne ka tarika </h1>
+  </div>
+  )
+  
+}
+function Demo2(){
+  return(
+  <div>
+    <h1> isko bhi kr lete hai runn  </h1>
+  </div>
+  )
+}
+/* DIFFERENCE BETWEEN FUNCTION AND COMPONENT */
+function Comp(){            /* DIFFERENCE BETWEEN FUNCTION AND COMPONENT */
+  return(
+    <div>
+      <h1>this is component</h1>
+      <h1>iske ander hum html tags bhi use kr skte hai<br></br> andddd naam ka pehla letter capital </h1>
+    </div>
+    
+  )
+}
+function func(){
+  /*console.log(" so this is a function where we can we js , for function the first name of letter is in small 
+  and that is how compiler difference between them")*/      
+  /*yeh isiliye use nhi krra kyuki console log browser ke console pe krtaa haii na ki screen pe */
 
-export default App
+  return " so this is a function where we can we js , for function the first name of letter is in small and that is how compiler difference between them" ;
+}
+export default App;
