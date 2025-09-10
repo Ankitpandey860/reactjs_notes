765f76b94202492ab8a2bd62910cd99cf57fead9
ankitpadey860 <ankitp6040@gmail.com>
2025-09-10 20:29:38 +0530

    rules for states

---
+++ src/rulesforstate.jsx
@@ -0,0 +1,4 @@
+/*do not call hooks inside conditions or loops
+do not call hooks after aconditional return statement 
+do not call hooks in classes components 
+do not call hooks inside try/catch/finallyblock*/
--- src/useactionstate.jsx
+++ src/useactionstate.jsx
@@ -6,10 +6,10 @@ function UseAction(){
         let password=FormData.get('password')
         await new Promise(res=>setTimeout(res,2000))
         if(name&&password){
-            return{message:'Data submitted'}
+            return{message:'Data submitted',name,password}
         }
         else{
-            return {error:'failed to submit.enter proper data'}
+            return {error:'failed to submit.enter proper data',name,password}
         }
     }
     const [data,action,pending]=useActionState(handleSubmit,undefined)
@@ -23,14 +23,16 @@ function UseAction(){
                 <input type="password" placeholder="enter password" name='password' />
                 <br/> <br/>
                 <button disabled={pending}>submit</button>
-                {
+                
+
+            </form>
+            {
                     data?.error&&<span>{data?.error}</span>
                 }
                 {
                     data?.message&&<span>{data?.message}</span>
                 }
-
-            </form>
+                <h3>name :{data?.name}</h3>
             
         </div>
     )
